97e0d7d70778cbb5ca89256237387dc6dd0eb451
Alex Ye <alexzye1@gmail.com>
2016-12-05 00:36:35 -0800

    Add scripts to populate the Players and Teams tables

---
+++ script/create-teams.sql
@@ -0,0 +1,68 @@
+/*
+   Populates the Teams table.
+
+   Authors:
+   Alex Ye, aye01
+   Robert Franklin Mathews IV, rfmathew
+   Esteban Ray Ramos, eramos04
+*/
+
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('ATL','Atlanta Hawks','Atlanta','GA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('BAL','Baltimore Bullets','Baltimore','MD');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('BOS','Boston Celtics','Boston','MA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('BUF','Buffalo Braves','Buffalo','NY');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CAP','Capital Bullets','Washington','DC');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CHA','Charlotte Bobcats','Charlotte','NC');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CHI','Chicago Bulls','Chicago','IL');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CHP','Chicago Packers','Chicago','IL');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CHZ','Chicago Zephyrs','Chicago','IL');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CIN','Cincinnati Royals','Cincinnati','OH');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CLE','Cleveland Cavaliers','Cleveland','OH');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('DAL','Dallas Mavericks','Dallas','TX');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('DEN','Denver Nuggets','Denver','CO');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('DET','Detroit Pistons','Detroit','MI');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('FTW','Fort Wayne Pistons','Fort Wayne','MI');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('GSW','Golden State Warriors','Oakland','CA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('HOU','Houston Rockets','Houston','TX');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('IND','Indiana Pacers','Indianapolis','IN');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('KCK','Kansas City Kings','Kansas City','KS');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('KCO','Kansas City-Omaha Kings','Kansas City','KS');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('LAC','Los Angeles Clippers','Los Angeles','CA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('LAL','Los Angeles Lakers','Los Angeles','CA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('MEM','Memphis Grizzlies','Memphis','TN');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('MIA','Miami Heat','Miami','FL');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('MIL','Milwaukee Bucks','Milwaukee','WI');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('MIN','Minnesota Timberwolves','Minneapolis','MN');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('MLH','Milwaukee Hawks','Milwaukee','WI');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('MPL','Minneapolis Lakers','Minneapolis','MN');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('NJN','New Jersey Nets','New Jersey','NJ');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('NOH','New Orleans Hornets','New Orleans','LA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('NOJ','New Orleans Jazz','New Orleans','LA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('NOK','New Orleans-Oklahoma City Hornets','New Orleans','LA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('NOP','New Orleans Pelicans','New Orleans','LA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('NYK','New York Knicks','New York City','NY');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('NYN','New York Nets','New York City','NY');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('OKC','Oklahoma City Thunder','Oklahoma City','OK');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('ORL','Orlando Magic','Orlando','FL');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('PHI','Philadelphia 76ers','Philadelphia','PA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('PHO','Phoenix Suns','Phoenix','AR');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('PHW','Philadelphia Warriors','Philadelphia','PA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('POR','Portland Trail Blazers','Portland','OR');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('ROC','Rochester Royals','Rochester','NY');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('SAC','Sacramento Kings','Sacramento','CA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('SAS','San Antonio Spurs','San Antonio','TX');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('SDC','San Diego Clippers','San Diego','CA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('SDR','San Diego Rockets','San Diego','CA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('SEA','Seattle SuperSonics','Seattle','WA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('SFW','San Francisco Warriors','San Francisco','CA');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('STL','St. Louis Hawks','St. Louis','MO');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('SYR','Syracuse Nationals','Syracuse','NY');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('TOR','Toronto Raptors','Toronto','CN');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('TRI','Tri-Cities Blackhawks','Moline','IL');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('UTA','Utah Jazz','Salt Lake City','UT');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('VAN','Vancouver Grizzlies','Vancouver','CN');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('WAB','Washington Bullets','Washington','DC');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('WAS','Washington Wizards','Washington','DC');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('BRK','Brooklyn Nets','Brooklyn','NY');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('CHO','Charlotte Hornets','Charlotte','NC');
+INSERT INTO Teams(Abbrev,Name,City,State) VALUES ('TOT','Traded Teams',NULL,NULL);
