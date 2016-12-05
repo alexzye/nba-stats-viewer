17280338de9e98cbb8e42e964d8c0eb10c1923bd
Alex Ye <alexzye1@gmail.com>
2016-12-05 00:59:28 -0800

    Add population scripts for champions, mvp table

---
+++ script/create-champions.sql
@@ -0,0 +1,20 @@
+/*
+   Populates the champions table.
+
+   Authors:
+   Alex Ye, aye01
+   Robert Franklin Mathews IV, rfmathew
+   Esteban Ray Ramos, eramos04
+*/
+
+INSERT INTO Champions(Team,Year) VALUES ('CLE',2016);
+INSERT INTO Champions(Team,Year) VALUES ('GSW',2015);
+INSERT INTO Champions(Team,Year) VALUES ('SAS',2014);
+INSERT INTO Champions(Team,Year) VALUES ('MIA',2013);
+INSERT INTO Champions(Team,Year) VALUES ('MIA',2012);
+INSERT INTO Champions(Team,Year) VALUES ('DAL',2011);
+INSERT INTO Champions(Team,Year) VALUES ('LAL',2010);
+INSERT INTO Champions(Team,Year) VALUES ('LAL',2009);
+INSERT INTO Champions(Team,Year) VALUES ('BOS',2008);
+INSERT INTO Champions(Team,Year) VALUES ('SAS',2007);
+INSERT INTO Champions(Team,Year) VALUES ('MIA',2006);
---
+++ script/create-mvp.sql
@@ -0,0 +1,19 @@
+/*
+   Populates the MVP table.
+
+   Authors:
+   Alex Ye, aye01
+   Robert Franklin Mathews IV, rfmathew
+   Esteban Ray Ramos, eramos04
+*/
+
+INSERT INTO MVP(Player,Team,Year) VALUES ('Stephen Curry','GSW',2016);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Stephen Curry','GSW',2015);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Kevin Durant','OKC',2014);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Lebron James','MIA',2013);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Lebron James','MIA',2012);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Derrick Rose','CHI',2011);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Lebron James','CLE',2010);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Lebron James','CLE',2009);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Kobe Bryant','LAL',2008);
+INSERT INTO MVP(Player,Team,Year) VALUES ('Dirk Nowitzki','DAL',2007);
