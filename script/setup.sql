391487974bb48e0942c436eede8c051788b55178
Alex Ye <alexzye1@gmail.com>
2016-12-05 12:03:29 -0800

    Add setup files for developing Spring GUI

---
+++ script/setup.sql
@@ -0,0 +1,4 @@
+source create-teams.sql;
+source create-players.sql;
+source create-mvp.sql;
+source create-champtions.sql;
---
+++ src/build
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -cp ./jdbc.jar:. NbaData.java SpringUtilities.java
