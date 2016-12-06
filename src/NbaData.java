9f01c0782e54e50d73df7d7e2fb94e9e668dcaf2
Alex Ye <alexzye1@gmail.com>
2016-12-05 19:21:16 -0800

    Change default to fullscreen on app start

--- src/NbaData.java
+++ src/NbaData.java
@@ -1,39 +1,3 @@
-/*
- * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */ 
-
-
-
-/*
- * NbaData.java requires SpringUtilities.java
- */
 import java.sql.*;
 import javax.swing.*;
 import javax.swing.event.*;
@@ -45,7 +9,7 @@ import java.awt.Component;
 import java.util.Vector;
 
 public class NbaData extends JPanel {
-    private boolean DEBUG = false;
+    private boolean DEBUG = true;
     private JTable table;
     private JTextField filterText;
     private JTextField statusText;
@@ -57,20 +21,19 @@ public class NbaData extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         makeConnection();
+        
         ResultSet result = null;
-        try {
-            Statement s = conn.createStatement();
-            result = s.executeQuery("SELECT * FROM Players");
-            System.out.println(result);
-        } catch (SQLException e) {
-
-        };
         DefaultTableModel model = null;
+        Statement s = null;
+        
+        try {
+            s = conn.createStatement();
+            result = s.executeQuery("SELECT * FROM Players");
+        } catch (SQLException e) {};
+        
         try {
             model = buildTableModel(result);    
-        } catch(SQLException e) {
-
-        }
+        } catch(SQLException e) {}
         
         sorter = new TableRowSorter<DefaultTableModel>(model);
         table = new JTable(model);
@@ -85,24 +48,22 @@ public class NbaData extends JPanel {
         //When selection changes, provide user with row numbers for
         //both view and model.
         table.getSelectionModel().addListSelectionListener(
-                new ListSelectionListener() {
-                    public void valueChanged(ListSelectionEvent event) {
-                        int viewRow = table.getSelectedRow();
-                        if (viewRow < 0) {
-                            //Selection got filtered away.
-                            statusText.setText("");
-                        } else {
-                            int modelRow = 
-                                table.convertRowIndexToModel(viewRow);
-                            statusText.setText(
-                                String.format("Selected Row in view: %d. " +
-                                    "Selected Row in model: %d.", 
-                                    viewRow, modelRow));
-                        }
+            new ListSelectionListener() {
+                public void valueChanged(ListSelectionEvent event) {
+                    int viewRow = table.getSelectedRow();
+                    if (viewRow < 0) {
+                        //Selection got filtered away.
+                        statusText.setText("");
+                    } else {
+                        int modelRow = table.convertRowIndexToModel(viewRow);
+                        statusText.setText(
+                            String.format("Selected Row in view: %d. " +
+                                "Selected Row in model: %d.", 
+                                viewRow, modelRow));
                     }
                 }
+            }
         );
-
 
         //Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
@@ -193,9 +154,9 @@ public class NbaData extends JPanel {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("NbaData");
+        JFrame frame = new JFrame("NBA Data Viewer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         //Create and set up the content pane.
         NbaData newContentPane = new NbaData();
         newContentPane.setOpaque(true); //content panes must be opaque
